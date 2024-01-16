b402bc0cef12e8d2375c9a3d94d656197592b817
Ali Abedi <aliabedi@dornica.net>
2024-01-16 12:12:21 +0330

    add dda method to helpers

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,1 +1,10 @@
 <?php
+
+if (!function_exists('dda')) {
+    function dda(mixed ...$parameters)
+    {
+        foreach ($parameters as $value)
+            dump(($value instanceof Arrayable) ? $value->toArray() : $value);
+        exit(1);
+    }
+}
